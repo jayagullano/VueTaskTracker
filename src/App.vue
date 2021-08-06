f30db7e28b0aaa4c58cfbc67b30a7e467aed892e
jayagullano <jayagullano@gmail.com>
2021-08-05 23:38:34 -0400

    Configured UI to communicate with backend.
    This implements methods to communicate with the json-server to simulate a communication with the REST API.

--- src/App.vue
+++ src/App.vue
@@ -38,30 +38,67 @@ export default {
     toggleAddTask(){
       this.showAddTask = !this.showAddTask; //Toggles from false to true
     },
-    addTask(task){
-      task = {...task, id: this.tasks.length+1}; //Sets the id of the new task
-      this.tasks = [...this.tasks, task]; //Appends new object to array
+    async addTask(task){
+      //task = {...task, id: this.tasks.length+1}; //POSTs default increment the id
+
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task)
+      });
+
+      const data = await res.json(); 
+      
+      this.tasks = [...this.tasks, data]; //Appends new object to array
     },
-    deleteTask(id){
+    async deleteTask(id){
       //console.log('task:',id);
 
       if(confirm('Are you sure you want to delete?')){
-        this.tasks = this.tasks.filter((task) => {
-          return (task.id != id);
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
         });
+
+        res.status === 200 //Success code 
+        ? 
+        (this.tasks = this.tasks.filter((task) => {
+          return (task.id != id);
+        })) 
+        : alert('Error when deleting task');
+
       }
     },
-    toggleReminder(id){
+    async toggleReminder(id){
       //Flips reminder boolean for selected id
+      const taskToToggle = await this.fetchTask(id);
+      const updated = {...taskToToggle, reminder: !taskToToggle.reminder};
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updated)
+      });
+
+      const data = await res.json();
+
       this.tasks = this.tasks.map((task) => {
         if(task.id == id){
-          return {...task, reminder: !task.reminder}
+          return {...task, reminder: data.reminder}
         }
         return task;
       })
     },
     async fetchTasks(){
-      const res = await fetch('http://localhost:1000/tasks');
+      const res = await fetch('api/tasks');
+      const data = await res.json();
+      return data;
+    },
+    async fetchTask(id){
+      const res = await fetch(`api/tasks/${id}`);
       const data = await res.json();
       return data;
     }
